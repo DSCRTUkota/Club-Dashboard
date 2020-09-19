4fb83b9df41feb8f63e7731d34b1171f1306e3e8
davedeveloper <ddave2001@gmail.com>
2020-09-19 13:35:23 +0530

    Add the files for the first time

---
+++ FrontEnd/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MiniDrawer from './Components/App.js';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MiniDrawer/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
